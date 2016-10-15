2efc2c010c3bbf4bb98c0a4894ca1e97df04369b
Pragma0nce <kobus.coetzeee@gmail.com>
2016-10-15 23:32:55 +0200

    Update comic books controller to use repisoitory

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,27 +10,24 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                SeriesTitle = "The Amazing spider man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final Issue! ma niiiiiiga</p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Name="Dan Slott",Role="Script" },
-                    new Artist() {Name="Ramos",Role="Penicls" },
-                    new Artist() {Name="Victor",Role="Inks" },
-                    new Artist() {Name="Edgar",Role="Colors" },
-                    new Artist() {Name="Chris",Role="Letters" },
-                }
+                return HttpNotFound();
+            }
 
-
-            };
-
-
+            var comicBook = _comicBookRepository.GetComicBook((id.Value));
             return View(comicBook);
         }
+    
     }
 }
